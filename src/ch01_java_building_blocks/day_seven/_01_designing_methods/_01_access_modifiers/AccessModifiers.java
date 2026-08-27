993be4e215bd73da24ad1be839ec65354e9c2e18
SamuelChavez <157629722+Samuelichu@users.noreply.github.com>
2026-08-27 01:31:43 -0500

    feat(*): add exercises day seven and resolve test

---
+++ src/ch01_java_building_blocks/day_seven/_01_designing_methods/_01_access_modifiers/AccessModifiers.java
@@ -0,0 +1,8 @@
+package ch01_java_building_blocks.day_seven._01_designing_methods._01_access_modifiers;
+
+public class AccessModifiers {
+    public void walk1(){}
+    //default void walk2 (){} no es un modificador de acceso valido
+    //void public walk3(){} el modificador de acceso debe ir antes del tipo que va a retornar el metodo
+    void walk4(){}
+}
